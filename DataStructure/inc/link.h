5eab506ad85a8bd8c96de10c0e21a0e8d85f48f1
wangfeng0301 <fengwang0301@163.com>
2020-12-07 19:51:48 +0800

    增加单向链表翻转

--- DataStructure/inc/link.h
+++ DataStructure/inc/link.h
@@ -18,7 +18,7 @@ void InsertNode(LinkNode *list,int n);//在第n个节点前插入新节点
 void ChangeNodeValue(LinkNode * list,int n);//修改第n个节点的值
 void DeleteNote(LinkNode *list,int n);//删除第n个节点
 void DeleteLinkList(LinkNode *list);//删除链表
-
-void testLink();
+void ReverseLinkList(LinkNode *list);//翻转链表
+void testLink(void);
 
 #endif
