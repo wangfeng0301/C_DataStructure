38c97637d9c23495cbeec1f3edbe6c58f535e4cd
wangfeng0301 <fengwang0301@163.com>
2021-01-13 14:04:28 +0800

    增加链式队列

--- DataStructure/inc/queue.h
+++ DataStructure/inc/queue.h
@@ -17,11 +17,27 @@ typedef struct
 	QueueData_t *qu;//数组存放数据
 }Queue_t;
 
+/*链式队列节点结构体*/
+typedef struct LinkQueueNode
+{
+	QueueData_t data;
+	struct LinkQueueNode* next;
+}LinkQueueNode_t;
+/* 链式队列结构体 */
+typedef struct 
+{
+	int size;//队列大小
+	LinkQueueNode_t *front;//队头
+	LinkQueueNode_t *rear;//队尾
+	//LinkQueueNode_t *qu;//数组存放数据
+}LinkQueue_t;
+
 void CreateQueue(Queue_t *q,int size);
 bool EnQueue(Queue_t *q,QueueData_t dat);
 bool DeQueue(Queue_t *q, QueueData_t *dat);
 void ClearQueue(Queue_t *q);
 
 void testQueue(void);
+void testLinkQueue(void);
 
 #endif
--- DataStructure/src/queue.c
+++ DataStructure/src/queue.c
@@ -65,7 +65,12 @@ bool DeQueue(Queue_t *q, QueueData_t *dat)
 	}
 }
 
-//清空队列
+/************************************************************************
+ *功能：清空队列
+ *输入：q:队列指针	
+ *输出：无
+ *返回：无
+************************************************************************/
 void ClearQueue(Queue_t *q)
 {
 	q->front = q->rear = 0;
@@ -79,9 +84,86 @@ void ClearQueue(Queue_t *q)
  *输出：q:队列指针
  *返回：无
 ************************************************************************/
-void CreateLinkQueue(Queue_t *q)
+void CreateLinkQueue(LinkQueue_t *q)
 {
+	/* 为减少访问队首节点的时间代价，不使用专用表头虚节点 */
+	q->size = 0;
+	q->front = q->rear = NULL;
+}
+/************************************************************************
+ *功能：进队列
+ *输入：q:队列指针
+ *		dat:进队列的数据
+ *输出：q:队列指针
+ *返回：TRUE or FLASE
+************************************************************************/
+bool EnLinkQueue(LinkQueue_t *q,QueueData_t dat)
+{
+	LinkQueueNode_t *temp;
 
+	temp = (LinkQueueNode_t*)malloc(sizeof(LinkQueueNode_t));//申请新节点
+	if(temp == NULL)
+	{
+		printf("申请内存失败r\n");
+		exit(-1);
+	}
+	temp->data = dat;//新节点赋值
+	temp->next = NULL;
+	if(q->rear == NULL)//队列为空
+	{	
+		q->front = q->rear = temp;
+	}
+	else
+	{
+		q->rear->next = temp;//新节点加在队尾
+		q->rear = q->rear->next;
+	}
+	q->size ++;
+	return TRUE;
+}
+/************************************************************************
+ *功能：出队列
+ *输入：q:队列指针	
+ *输出：dat:出队列的数据
+ *返回：TRUE or FLASE
+************************************************************************/
+bool DeLinkQueue(LinkQueue_t *q, QueueData_t *dat)
+{
+	LinkQueueNode_t *temp;
+
+	if(q->rear == NULL)
+	{
+		printf("队列为空\r\n");
+		return FALSE;
+	}
+	*dat = q->front->data;//取数据
+	temp = q->front;
+	q->front = q->front->next;
+	free(temp);//释放取出的节点
+	temp = NULL;
+	if(q->front == NULL)
+		q->rear = NULL;
+	q->size --;
+	return TRUE;
+}
+/************************************************************************
+ *功能：清空队列
+ *输入：q:队列指针	
+ *输出：无
+ *返回：无
+************************************************************************/
+void ClearLinkQueue(LinkQueue_t *q)
+{
+	LinkQueueNode_t *temp;
+	while(q->front != NULL)
+	{
+		temp = q->front;
+		free(temp);
+		q->front = q->front->next;
+	}
+	q->rear = NULL;
+	q->size = 0;
+	printf("清空队列成功\r\n");
 }
 
 void testQueue(void)
@@ -108,6 +190,34 @@ void testQueue(void)
 	}
 	ClearQueue(&q);
 	if(TRUE == DeQueue(&q,&dat))
+		printf("%d\r\n",dat.element);
+	
+}
+
+void testLinkQueue(void)
+{
+	LinkQueue_t q;
+	QueueData_t dat;
+	int i;
+
+	CreateLinkQueue(&q);
+	dat.element = 10;
+	EnLinkQueue(&q,dat);
+	dat.element = 20;
+	EnLinkQueue(&q,dat);
+	dat.element = 30;
+	EnLinkQueue(&q,dat);
+	dat.element = 40;
+	EnLinkQueue(&q,dat);
+	dat.element = 50;
+	EnLinkQueue(&q,dat);
+	for(i = 0;i<5;i++)
+	{
+		if(TRUE == DeLinkQueue(&q,&dat))
+			printf("%d\r\n",dat.element);
+	}
+	ClearLinkQueue(&q);
+	if(TRUE == DeLinkQueue(&q,&dat))
 		printf("%d\r\n",dat.element);
 	
 }
