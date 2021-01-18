ea57fd6a3719e3b222c247d97665c90f5117f020
wangfeng0301 <fengwang0301@163.com>
2021-01-18 19:03:16 +0800

    C库函数<String.h>源码，未完待续。。。

--- DataStructure/inc/string_my.h
+++ DataStructure/inc/string_my.h
@@ -4,6 +4,9 @@
 void *Mymemchr(const void *str, int c, size_t n);
 int Mymemcmp(const void *str1, const void *str2, size_t n);
 void *Mymemcpy(void *str1, const void *str2, size_t n);
+char *Mystrcat(char *dest, const char *src);
+char *Mystrncat(char *dest, const char *src, size_t n);
+char *Mystrchr(const char *str, int c);
 
 int Mystrlen(char * str);					//计算字符串长度
 char *Mystrcpy(char *str1,char *str2);		//将字符串2复制到字符串1中
--- DataStructure/src/string_my.c
+++ DataStructure/src/string_my.c
@@ -89,6 +89,65 @@ void *Mymemcpy(void *str1, const void *str2, size_t n)
 	}
 	return str1;
 }
+/************************************************************************
+ *功能：src 所指向的字符串追加到 dest 所指向的字符串的结尾
+ *输入：src:指向要追加的字符串，该字符串不会覆盖目标字符串。
+ *		dest:指向目标数组，该数组包含了一个 C 字符串，且足够容纳追加后的字符串。
+ *输出：无
+ *返回：返回一个指向最终的目标字符串 dest 的指针。
+************************************************************************/
+char *Mystrcat(char *dest, const char *src)
+{
+	char *temp = (char *)src;
+	char *temp2 = dest;
+	int len = strlen(dest);//目标串大小
+	while(*temp != 0)
+		temp2[len++] = *temp++;
+	temp2[len] = 0;//加结束符
+	return dest;
+}
+/************************************************************************
+ *功能：把 src 所指向的字符串追加到 dest 所指向的字符串的结尾，直到 n 字符长度为止。
+ *输入：src:要追加的字符串。
+ *		dest:指向目标数组，该数组包含了一个 C 字符串，且足够容纳追加后的字符串，包括额外的空字符。
+ *		n:要追加的最大字符数。
+ *输出：无
+ *返回：返回一个指向最终的目标字符串 dest 的指针。
+************************************************************************/
+char *Mystrncat(char *dest, const char *src, size_t n)
+{
+	char *temp = (char *)src;
+	char *temp2 = dest;
+	int len = strlen(dest);//目标串大小
+	while(n --)
+		temp2[len++] = *temp++;
+	temp2[len] = 0;//加结束符
+	return dest;
+}
+/************************************************************************
+ *功能：在参数 str 所指向的字符串中搜索第一次出现字符 c（一个无符号字符）的位置。
+ *输入：src:要被检索的 C 字符串。
+ *		c:在 str 中要搜索的字符。
+ *输出：无
+ *返回：返回在字符串 str 中第一次出现字符 c 的位置，如果未找到该字符则返回 NULL。
+************************************************************************/
+char *Mystrchr(const char *str, int c)
+{
+	char *temp;
+	if(str == NULL)
+	{
+		printf("字符串为空\r\n");
+		exit(-1);
+	}
+	temp = (char *)str;
+	while(*temp != 0)
+	{
+		if(*temp == c)
+			return temp;
+		temp ++;
+	}
+	return NULL;
+}
 //计算字符串有效长度,不计结束符
 int Mystrlen(char * str)
 {
@@ -194,6 +253,7 @@ void testString(void)
 	char ch = 'c';
 	int ret;
 	char *retchar;
+	char str5[50] = {"12345"};
 
 	printf("Mymemchr test\r\n");
 	retchar = (char*)Mymemchr(str1, ch, strlen(str1));
@@ -212,6 +272,24 @@ void testString(void)
 	printf("使用 memcpy 前: %s\n", str1);
 	Mymemcpy(str1, str2, strlen(str2));
 	printf("使用 memcpy 后: %s\n", str1);
+
+	printf("\r\nMystrcat test\r\n");
+	printf("str5:%s\r\n",str5);
+	printf("str1:%s\r\n",str1);
+	retchar = Mystrcat(str5,str1);
+	printf("链接str5和str1：%s\r\n",retchar);
+
+	printf("\r\nMystrncat test\r\n");
+	printf("str5:%s\r\n",str5);
+	printf("str1:%s\r\n",str1);
+	retchar = Mystrncat(str5,str1,5);
+	printf("链接str5和str1：%s\r\n",retchar);
+
+	printf("\r\nMystrchr test\r\n");
+	printf("str5:%s\r\n",str5);
+	ch = '5';
+	retchar = (char*)Mystrchr(str5, ch);
+	printf("[%s]中[%c] 之后的字符串是 - [%s]\n", str5, ch, retchar);
 
 	printf("str1:%s\r\n",str1);
 	printf("str2:%s\r\n",str2);
